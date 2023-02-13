dd88da978b3d3919ce998d8c4eb9ee86fe068341
A-Nasser-Unity <108767996+A-Nasser-Unity@users.noreply.github.com>
2023-02-13 03:06:17 +0200

    update

--- Assets/Thirdweb/Examples/Scripts/Prefabs/ThirdwebManager.cs
+++ Assets/Thirdweb/Examples/Scripts/Prefabs/ThirdwebManager.cs
@@ -57,7 +57,13 @@ public class ThirdwebManager : MonoBehaviour
             Destroy(gameObject);
 
 #if !UNITY_EDITOR
-        SDK = new ThirdwebSDK(chainIdentifiers[chain]);
+        SDK = new ThirdwebSDK(chainIdentifiers[chain], new ThirdwebSDK.Options() {
+            gasless = new ThirdwebSDK.GaslessOptions() {
+                    openzeppelin = new ThirdwebSDK.OZDefenderOptions() {
+                        relayerUrl = "https://api.defender.openzeppelin.com/autotasks/20973345-f104-4682-9bb9-2edf8849c5c3/runs/webhook/8db4ba89-3c75-4a75-9f0e-98d36b4337a3/Gr71P9nk6hnh1SapzHoYGw"
+                    }
+            }
+        });
 #endif
     }
 }
